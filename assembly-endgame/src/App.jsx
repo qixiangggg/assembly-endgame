48342d187fa7926680f107b528fd2a40f11c6d27
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-06-26 00:08:54 +0800

    Creating layout: - word - keyboard - New game button

--- assembly-endgame/src/App.jsx
+++ assembly-endgame/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import { languages } from './languages'
 export default function App() {
+  const [currentWord, setCurrentWord] = useState("react") 
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
   const languageElements = languages.map(language => (
       <span 
         style={{backgroundColor: language.backgroundColor,
@@ -10,6 +14,20 @@ export default function App() {
         {language.name}
       </span>
     ))
+  const currentWordElements = currentWord.toUpperCase().split("").map((letter,index) =>
+    (
+      <span key={index}>
+        {letter}
+      </span>
+    )
+  )
+
+  const alphabetElements = alphabet.toUpperCase().split("").map(letter =>
+  (
+    <button key={letter}>
+      {letter}
+    </button>
+  ))
   return (
     <main>
       <header>
@@ -23,6 +41,15 @@ export default function App() {
       <section className="languages">
         {languageElements}
       </section>
+      <section className='word'>
+        {currentWordElements}
+      </section>
+      <section className='alphabet'>
+        {alphabetElements}
+      </section>
+      <button className='new-game'>
+        New Game
+      </button>
     </main>
   )
 }
